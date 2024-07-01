0ed22e8db4714a1cac09029f28d9f89992dc2ebd
Hector Rangel <hector.rangel948@gmail.com>
2024-06-30 21:19:33 -0600

    Proyecto recursos humanos front end y back end con react y spring boot

---
+++ recursos-humanos-app/src/App.js
@@ -0,0 +1,21 @@
+import Navegacion from "./plantilla/Navegacion";
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
+import EditarEmpleado from "./empleados/EditarEmpleado";
+function App() {
+  return (
+    <div className="container">
+        <BrowserRouter>
+          <Navegacion/>
+          <Routes>
+            <Route exact path="/" element={<ListadoEmpleados/>}/>
+            <Route exact path="/agregar" element={<AgregarEmpleado/>}/>
+            <Route exact path="/editar/:id" element={<EditarEmpleado/>}/>
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
